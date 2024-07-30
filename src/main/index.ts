f2aad5681546d8c98680f24c40a96db0821e4f45
TomiPr <tomyprastiyo8@gmail.com>
2024-07-30 11:36:53 +0700

    [add] keep alive bot

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,11 +2,14 @@ import { createDiscordClient } from "../infrastructure/discord/DiscordClient";
 import { BotService } from "../application/services/BotService";
 import { CommandRepository } from "../domain/repositories/CommandRepository";
 import { config } from "../infrastructure/config/config";
+import { keepAlive } from "../infrastructure/discord/keepAlive";
 
 const client = createDiscordClient();
 const commandRepository = new CommandRepository();
 const botService = new BotService(client, commandRepository);
 
 botService.init();
+
+keepAlive();
 
 client.login(config.token);
